60c7e750ea492eaea9b2da9170b7798aa2b12e35
Theerawat Ramchuen <ramchuen@gmail.com>
2019-07-06 14:17:20 +0700

    Add files via upload

--- trainer.py
+++ trainer.py
@@ -1,26 +1,7 @@
 
 # coding: utf-8
 
-# ## Tutorial Keras: Transfer Learning with ResNet50 for image classification on Cats & Dogs dataset
-# 
-# ### Suni Kumar
-
-# This kernel is intended to be a tutorial on Keras around image files handling for Transfer Learning using pre-trained weights from ResNet50 convnet.
-# 
-# Though loading all train & test images resized (224 x 224 x 3) in memory would have incurred ~4.9GB of memory, the plan was to batch source image data during the training, validation & testing pipeline. Keras ImageDataGenerator supports batch sourcing image data for all training, validation and testing. Actually, it is quite clean and easy to use Keras ImageDataGenerator except few limitations (listed at the end).
-# 
-# Keras ImageDataGenerator expects labeled training images to be available in certain folder heirarchy, 'train' data was manually split into 10k for training & 2.5k for validation and re-arranged into the desired folder hierarchy. Even 'test' images had to rearranged due to a known issue in flow_from_directory.
-
-
-
-#import numpy as np
-#import pandas as pd
-
 import matplotlib.pyplot as plt
-
-#import cv2
-
-#import os
 
 # ### Global Constants
 # Fixed for our Cats & Dogs classes
@@ -38,18 +19,18 @@ OBJECTIVE_FUNCTION = 'categorical_crossentropy'
 LOSS_METRICS = ['accuracy']
 
 # EARLY_STOP_PATIENCE must be < NUM_EPOCHS
-NUM_EPOCHS = 50
+NUM_EPOCHS = 20
 EARLY_STOP_PATIENCE = 3
 
 # These steps value should be proper FACTOR of no.-of-images in train & valid folders respectively
 # Training images processed in each step would be no.-of-train-images / STEPS_PER_EPOCH_TRAINING
-STEPS_PER_EPOCH_TRAINING = 3500
-STEPS_PER_EPOCH_VALIDATION = 700
+STEPS_PER_EPOCH_TRAINING = 4030
+STEPS_PER_EPOCH_VALIDATION = 1009
 
 # These steps value should be proper FACTOR of no.-of-images in train & valid folders respectively
 # NOTE that these BATCH* are for Keras ImageDataGenerator batching to fill epoch step input
-BATCH_SIZE_TRAINING = 100
-BATCH_SIZE_VALIDATION = 100
+BATCH_SIZE_TRAINING = 32
+BATCH_SIZE_VALIDATION = 32
 
 # Using 1 to easily manage mapping between test_generator & prediction for submission preparation
 BATCH_SIZE_TESTING = 1
@@ -59,30 +40,8 @@ from tensorflow.python.keras.applications import ResNet50
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Dense
 
-### 
-### Below systax is available with TensorFlow 1.11 onwards but this upgrade is not available for Kaggle kernel yet
-###
-#import tensorflow as tf
-#print(tf.__version__)
-#import tensorflow as tf
-#from tf.keras.applications import ResNet50
-#from tf.keras.models import Sequential
-
-
-# ### ResNet50
-# * Notice that resnet50 folder has 2 pre-trained weights files... xyz_tf_kernels.h5 & xyz_tf_kernels_NOTOP.h5
-# * The xyz_tf_kernels.h5 weights is useful for pure prediction of test image and this prediction will rely completely on ResNet50 pre-trained weights, i.e., it does not expected any training from our side
-# * Out intention in this kernel is Transfer Learning by using ResNet50 pre-trained weights except its TOP layer, i.e., the xyz_tf_kernels_NOTOP.h5 weights... Use this weights as initial weight for training new layer using train images
-
-
+#### ResNet50
 resnet_weights_path = 'resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5'
-
-
-# ### Define Our Transfer Learning Network Model Consisting of 2 Layers
-# 
-# Here, we are preparing specification or blueprint of the TensorFlow DAG (directed acyclcic graph) for just the MODEL part.
-
-#Still not talking about our train/test data or any pre-processing.
 
 model = Sequential()
 
@@ -100,24 +59,14 @@ model.summary()
 
 # ### Compile Our Transfer Learning Model
 
-
-
-
 from tensorflow.python.keras import optimizers
 
-sgd = optimizers.SGD(lr = 0.01, decay = 1e-6, momentum = 0.9, nesterov = True)
+sgd = optimizers.SGD(lr = 0.001, decay = 1e-6, momentum = 0.9, nesterov = True)
 model.compile(optimizer = sgd, loss = OBJECTIVE_FUNCTION, metrics = LOSS_METRICS)
 
 
 # ### Prepare Keras Data Generators
 # 
-# Keras *ImageDataGenerator(...)* generates batches of tensor image data with real-time data augmentation. The data will be looped over (in batches). It is useful with large dataset to source, pre-process (resize, color conversion, image augmentation, batch normalize) & supply resulting images in batches to downstream Keras modeling components, namely *fit_generator(...)* & *predict_generator(...)* -vs- *fit(...)* & *predict(...)* for small dataset.
-# 
-# Kaggle competition rule expects Dog & Cat to be labeled as 1 & 0. Keras >> ImageDataGenerator >> flow_from_directory takes in 'classes' list for mapping it to LABEL indices otherwise treats sub-folders enumerated classes in alphabetical order, i.e., Cat is 0 & Dog is 1.
-
-
-
-
 from keras.applications.resnet50 import preprocess_input
 from keras.preprocessing.image import ImageDataGenerator
 
@@ -179,8 +128,12 @@ fit_history = model.fit_generator(
         callbacks=[cb_checkpointer, cb_early_stopper]
 )
 
-model.load_weights("working/best.hdf5")
 
+# Load Saved_weight
+from pathlib import Path
+my_file = Path("working/best.hdf5")
+if my_file.is_file():
+    model.load_weights('working/best.hdf5')
 
 # ### Training Metrics
 # 
@@ -210,94 +163,3 @@ plt.legend(['train', 'valid'])
 
 plt.show()
 
-### In[35]:
-##
-##
-### NOTE that flow_from_directory treats each sub-folder as a class which works fine for training data
-### Actually class_mode=None is a kind of workaround for test data which too must be kept in a subfolder
-##
-### batch_size can be 1 or any factor of test dataset size to ensure that test dataset is samples just once, i.e., no data is left out
-#test_generator = data_generator.flow_from_directory(
-#    directory = 'blind/',
-#    target_size = (image_size, image_size),
-#    batch_size = BATCH_SIZE_TESTING,
-#    class_mode = None,
-#    shuffle = False,
-#    seed = 123
-#)
-##
-### Try batch size of 1+ in test_generator & check batch_index & filenames in resulting batches
-##'''
-##for i in test_generator:
-##    #print(test_generator.batch_index, test_generator.batch_size)
-##    idx = (test_generator.batch_index - 1) * test_generator.batch_size
-##    print(test_generator.filenames[idx : idx + test_generator.batch_size])
-##'''
-##
-##
-### ### Observe Prediction Time With Different Batch Size
-### 
-### With GPU, 97s for full prediction with batch_size=100 -vs- 264s with 1. But note that to avoid ImageDataGenerator iterator repeatability, we need to use 1 as batch_size.
-##
-### In[36]:
-##
-##
-### Reset before each call to predict
-#test_generator.reset()
-#
-#pred = model.predict_generator(test_generator, steps = len(test_generator), verbose = 1)
-#
-#predicted_class_indices = np.argmax(pred, axis = 1)
-#
-#
-### In[37]:
-#
-#
-#TEST_DIR = 'blind/'
-##f, ax = plt.subplots(10, 1, figsize = (50, 50))
-#
-#for i in range(0,10):
-##    imgBGR = cv2.imread(TEST_DIR + test_generator.filenames[i])
-##    imgRGB = cv2.cvtColor(imgBGR, cv2.COLOR_BGR2RGB)
-#    
-#    # a if condition else b
-#    predicted_class = "Reject" if predicted_class_indices[i] else "Accept"
-#
-##    ax[i//5, i%5].imshow(imgRGB)
-##    ax[i//5, i%5].axis('off')
-##    ax[i//5, i%5].set_title("Predicted:{}".format(predicted_class))    
-##
-##plt.show()
-#
-#
-### In[38]:
-#
-#
-#results_df = pd.DataFrame(
-#    {
-#        'id': pd.Series(test_generator.filenames), 
-#        'label': pd.Series(predicted_class_indices)
-#    })
-#results_df['id'] = results_df.id.str.extract('(\d+)')
-#results_df['id'] = pd.to_numeric(results_df['id'], errors = 'coerce')
-#results_df.sort_values(by='id', inplace = True)
-#
-#results_df.to_csv('submission.csv', index=False)
-#results_df.head()
-#
-#
-## ### Keras Limitations
-## 
-## * [10/02/2018] The *validation_split* is not supported in *fit_generator*, hence its expects ImageDataGenerator for pre-splitted train & valid.
-## * [10/02/2018] Model learning through *fit_generator* is not compatible for Sklearn *GridSearchCV* again *mostly* due to no support for *validation_split*.
-#
-## ### Followup Plan
-## 
-## 1. Scale and pad and avoid aspect ratio change of original image through Keras ImageDataGenerator pre-processing insfrastructure
-## 2. Image augmentation
-## 3. Pipeline
-## 4. Distributed ML for Grid Search on Spark Cluster
-#
-## ### References
-## 
-## 1. [Transfer Learning by Dan B](https://www.kaggle.com/dansbecker/transfer-learning)
